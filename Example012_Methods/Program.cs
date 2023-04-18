b3b52d947c576c302c27d28539b8101a8593074f
Diana Naumenko <diananaymenko@mail.ru>
2023-04-18 23:34:41 +0300

    Example012_Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -96,24 +96,58 @@ string text = " - Я думаю, - сказал князь, улыбаясь, - 
 //             012
 // s[3] // r
 
-string Replase(string text, char oldValue, char newValue)
+//string Replase(string text, char oldValue, char newValue)
+//{
+ //   string result = String.Empty;
+
+ //   int length = text.Length;
+ //   for (int i = 0; i < length; i++)
+ //   {
+  //      if (text[i] == oldValue) result = result + $"{newValue}";
+  //      else result = result + $"{text[i]}";
+ //   }
+
+ //   return result;
+//}
+
+//string newText = Replase(text, ' ', '|');
+//Console.WriteLine(newText);
+//Console.WriteLine( );
+//newText = Replase(newText, 'к', 'К');
+//Console.WriteLine(newText);
+//newText = Replase(newText, 'C', 'c');
+//Console.WriteLine(newText);
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
 {
-    string result = String.Empty;
+    int count = array.Length;
 
-    int length = text.Length;
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < count; i++)
     {
-        if (text[i] == oldValue) result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
+        Console.Write($"{array[i]} ");
     }
-
-    return result;
+    Console.WriteLine();
 }
 
-string newText = Replase(text, ' ', '|');
-Console.WriteLine(newText);
-Console.WriteLine( );
-newText = Replase(newText, 'к', 'К');
-Console.WriteLine(newText);
-newText = Replase(newText, 'C', 'c');
-Console.WriteLine(newText);
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        //
+       for (int j = i + 1; j < array.Length; j++)
+       {
+            if(array[j] < array[minPosition]) minPosition = j;
+       }
+       int temporary = array[i];
+       array[i] = array[minPosition];
+       array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
